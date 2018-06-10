56474586ffba18f14a0e4706c44f9b6dcadf8f4f
Михаил <VitAm1nKa@hotmail.com>
2018-06-11 03:50:44 +0700

    Add models

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -1,5 +1,6 @@
 import { ProductCategoryPath }  from './ProductCategory';
 import { ImageGallery }         from './ImageGallery';
+import { Image }                from './Image';
 
 export class UserProductModelPreferences {
     constructor(model = {}) {
@@ -115,3 +116,34 @@ export class ProductModel {
         return new Product();
     }
 }
+
+//  #region SmallData
+export class ProductModelSmall {
+    constructor(model = {}) {
+        this.id = model.id;
+        this.title = model.title;    
+        this.catalogPath = model.catalogPath;
+        this.image = new Image(model.image);
+
+        this.path = this.path.bind(this);
+    }
+
+    path() {
+        return `${this.catalogPath}/${this.id}`;
+    }
+}
+
+export class ProductSmall {
+    constructor(model = {}) {
+        this.productId = model.productId;
+        this.colorCode = model.colorCode;
+        this.patternCode = model.patternCode;
+        this.title = model.title;
+        this.listPrice = model.listPrice;
+        this.productModelId = model.productModelId;
+        this.image = new Image(model.image || {});
+        this.productCategoryPath = model.productCategoryPath;
+        this.path = `${this.productCategoryPath}/${this.productModelId}?cl=${this.colorCode}`;
+    }
+}
+//  #endregion
